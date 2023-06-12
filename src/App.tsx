0ab5c7fc94314f89f0ffcc8f94c61befe82f183a
Constantino <Okonemmanuelmma@gmail.com>
2023-06-12 23:20:24 +0100

    new idea

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import "./App.css";
 function App() {
   return (
     <div className="main">
-      <div className="sub"></div>
+      <div className="sub">
+        <div className="sun"></div>
+        <div className="planet"></div>
+      </div>
     </div>
   );
 }
